c1db3f059dde5a2bd2cf3a2c830d90aa378e3112
ngervasio <nge@hellofresh.com>
2022-08-15 16:26:36 +0200

    add composition to styled components

--- src/Button.js
+++ src/Button.js
@@ -1,46 +1,33 @@
 /** @jsxImportSource @emotion/react */
 import styled from "@emotion/styled";
 
-const Button = styled.button`
-  color: ${(props) => {
-    if (props.buttonType === "primary") {
-      return "#ffffff";
-    } else {
-      return "#067a46";
-    }
+const Button = styled.button(
+  ({ buttonType }) => `
+color: ${buttonType === "ghost" ? "#067a46" : "#fff"};
+display: inline-flex;
+vertical-align: text-bottom;
+align-items: center;
+justify-content: center;
+cursor: pointer;
+border-radius: 4px;
+font-size: 16px;
+font-family: Agrandir, Verdana, Geneva, sans-serif;
+text-transform: none;
+text-decoration: none;
+letter-spacing: 0.7px;
+height: 40px;
+width: 340px;
+border-style: solid;
+border-width: 1px;
+border-color: #067a46;
+background-color: ${buttonType === "ghost" ? "#fff" : "#067a46"};
+box-shadow: ${buttonType === "ghost" ? "" : "rgb(0 0 0 / 20%) 0px 8px 16px 0px"}
   }};
-  display: inline-flex;
-  vertical-align: text-bottom;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  border-radius: 4px;
-  font-size: 16px;
-  font-family: Agrandir, Verdana, Geneva, sans-serif;
-  text-transform: none;
-  text-decoration: none;
-  letter-spacing: 0.7px;
-  height: 40px;
-  width: 340px;
-  border-style: solid;
-  border-width: 1px;
-  border-color: #067a46;
-  background-color: ${(props) => {
-    if (props.buttonType === "primary") {
-      return "#067a46";
-    } else {
-      return "#ffffff";
-    }
-  }};
-  box-shadow: ${(props) => {
-    if (props.buttonType === "primary") {
-      return "rgb(0 0 0 / 20%) 0px 8px 16px 0px";
-    }
-  }};
-`;
+`
+);
 
-const RecipeButton = (props) => {
-  return <Button buttonType={props.buttonType}>{props.children}</Button>;
+const RecipeButton = ({ buttonType, children }) => {
+  return <Button buttonType={buttonType}>{children}</Button>;
 };
 
 export default RecipeButton;
--- src/DeliveryInfo.js
+++ src/DeliveryInfo.js
@@ -19,45 +19,39 @@ export default function DeliveryInfo() {
     />
   );
 
-  const SpanTextOne = styled.span`
-    color: rgb(6, 122, 70);
+  const SpanBase = styled.span`
+    color: #242424;
     line-height: 1.5;
-    font-family: "Source Sans Pro", "Trebuchet MS", Helvetica, sans-serif;
     font-size: 16px;
     font-weight: 400;
-    display: in-line;
+    font-family: "Source Sans Pro", "Trebuchet MS", Helvetica, sans-serif;
+  `;
+
+  const DeliveryStatus = styled(SpanBase)`
+    color: #067a46;
     padding-right: 8px;
   `;
 
-  const SpanTextTwo = styled.span`
-    line-height: 1.5;
-    font-family: "Source Sans Pro", "Trebuchet MS", Helvetica, sans-serif;
-    font-size: 16px;
-    font-weight: 400;
-    color: rgb(36, 36, 36);
-    display: in-line;
+  const DeliveryStatusLink = styled(SpanBase)`
     text-decoration: underline;
   `;
 
-  const SpanTextThree = styled.span`
-    font-family: Agrandir, Verdana, Geneva, sans-serif;
-    font-weight: 400;
-    letter-spacing: unset;
-    color: rgb(36, 36, 36);
+  const DeliveryInfoText = styled(SpanBase)`
     font-size: 24px;
     line-height: 32px;
+    font-family: Agrandir, Verdana, Geneva, sans-serif;
   `;
 
   return (
     <DeliveryInfo>
       <FlexBox direction="column">
         <FlexBox>
-          <SpanTextOne>DELIVERED</SpanTextOne>
-          <SpanTextTwo>Track your delivery here</SpanTextTwo>
+          <DeliveryStatus>DELIVERED</DeliveryStatus>
+          <DeliveryStatusLink>Track your delivery here</DeliveryStatusLink>
         </FlexBox>
-        <SpanTextThree>time to cook!</SpanTextThree>
+        <DeliveryInfoText>time to cook!</DeliveryInfoText>
       </FlexBox>
-      <RecipeButton>Help with delivery</RecipeButton>
+      <RecipeButton buttonType="ghost">Help with delivery</RecipeButton>
     </DeliveryInfo>
   );
 }
